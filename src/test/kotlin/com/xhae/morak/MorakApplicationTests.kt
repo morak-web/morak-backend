afa39542f2310cffecc776d5252e571b8fee03f3
woooo <99981511+xhae000@users.noreply.github.com>
2025-07-13 22:25:38 +0900

    Initial commit

---
+++ src/test/kotlin/com/xhae/morak/MorakApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xhae.morak
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MorakApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
